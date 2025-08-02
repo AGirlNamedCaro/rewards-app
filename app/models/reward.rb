8e104cc627802326fdd8232a0905d5df2d2c08b3
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2025-08-02 09:26:11 -0600

    added description max

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,6 @@
 class Reward < ApplicationRecord
   belongs_to :store, inverse_of: :rewards
+  has_one_attached :image
+
+  validates :description, length: { maximum: 30 }
 end
--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -3,5 +3,23 @@ require 'rails_helper'
 RSpec.describe Reward, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:store).inverse_of(:rewards) }
+    it { is_expected.to have_one_attached(:image) }
+  end
+
+  describe "validations" do
+    context "when description is valid" do
+      let(:reward) { create(:reward, description: "Get a small Enchilada, 50% off") }
+      it "is valid with a description of 30 characters" do
+        expect(reward).to be_valid
+      end
+    end
+
+    context "when description is not valid" do
+      let(:reward) { build(:reward, description: "Get a small Enchilada, 50% off Get a small Enchilada, 50% off") }
+      it "is invalid with a description longer than 30 characters" do
+        reward.valid?
+        expect(reward.errors[:description]).to include("is too long (maximum is 30 characters)")
+      end
+    end
   end
 end
