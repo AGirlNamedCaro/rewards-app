50658f8e030bbc37fe2c24d4766c59df452c3d73
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2025-08-02 16:19:42 -0600

    added dependent destroy

--- app/models/redemption.rb
+++ app/models/redemption.rb
@@ -1,5 +1,5 @@
 class Redemption < ApplicationRecord
   belongs_to :user, inverse_of: :redemptions
   belongs_to :reward
-  has_many :point_transactions, inverse_of: :redemption
+  has_many :point_transactions, inverse_of: :redemption, dependent: :destroy
 end
--- spec/models/redemption_spec.rb
+++ spec/models/redemption_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Redemption, type: :model do
   it { is_expected.to belong_to(:user).inverse_of(:redemptions) }
-  it { is_expected.to have_many(:point_transactions).inverse_of(:redemption) }
+  it { is_expected.to have_many(:point_transactions).inverse_of(:redemption).dependent(:destroy) }
 end
