1333804869822c2f865810ddfc8766d35602bf2a
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2025-08-02 09:27:08 -0600

    added images & updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,24 +21,41 @@ store2 = Store.create!(
   name: "Dunkin' Tim bits"
 )
 
-Reward.create!(
-  title: "Free Tacos",
-  description: "Get 3 free tacos at Taqueria del barrio",
-  points_required: 500,
-  store: store
-
+store3 = Store.create!(
+  name: "Pizza Bros"
 )
 
-Reward.create!(
-  title: "Free Coffee",
-  description: "Get an extra large coffee on us ",
-  points_required: 200,
-  store: store2
+store4 = Store.create!(
+  name: "Donut Den"
 )
 
-Reward.create!(
-  title: "Enchilada",
-  description: "Get a small Enchilada, 50% off",
-  points_required: 700,
-  store: store
-)
+rewards = [
+  { title: "Free Tacos", description: "Get 3 free tacos at Taqueria", points_required: 500, image_file: "tacos.jpg", store: store },
+  { title: "Free Coffee", description: "Extra large free coffee on us", points_required: 200, image_file: "ice-coffee.jpg", store: store2 },
+  { title: "Enchilada", description: "50% off small enchilada", points_required: 700, image_file: "enchilada.jpg", store: store },
+  { title: "Amazon Gift Card", description: "A $25 Amazon gift card.", points_required: 250, image_file: "amazon-gift-card.jpg", store: store },
+  { title: "Moka Pot", description: "30% off Moka pot", points_required: 900, image_file: "moka-pot.jpg", store: store2 },
+  { title: "Smoothie Voucher", description: "Free smoothie voucher", points_required: 150, image_file: "smoothie.jpg", store: store2 },
+  { title: "Pizza Slice", description: "1 pizza slice from Bros", points_required: 300, image_file: "pizza.jpg", store: store3 },
+  { title: "Movie Night", description: "1 movie ticket free", points_required: 400, image_file: "movie-ticket.jpg", store: store2 },
+  { title: "Donut Box", description: "6 assorted donuts box", points_required: 350, image_file: "donuts.jpg", store: store4 },
+  { title: "Free AirPods", description: "Free AirPods w/ purchase", points_required: 1000, image_file: "air-pods.jpg", store: store2 },
+  { title: "Burger Combo", description: "Free burger combo meal", points_required: 600, image_file: "burger-combo.jpg", store: store3 },
+  { title: "Yoga Class", description: "One free yoga session", points_required: 450, image_file: "yoga-class.jpg", store: store },
+  { title: "Gift Basket", description: "Holiday gift basket", points_required: 850, image_file: "gift-basket.jpg", store: store4 }
+]
+
+rewards.each do |data|
+  reward = Reward.create!(
+    title: data[:title],
+    description: data[:description],
+    points_required: data[:points_required],
+    store: data[:store]
+  )
+
+  reward.image.attach(
+    io: File.open(Rails.root.join("db/seeds/images/#{data[:image_file]}")),
+    filename: data[:image_file],
+    content_type: "image/#{File.extname(data[:image_file]).delete('.')}"
+  )
+end
--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :reward do
     store
     title { FFaker::Product.product_name }
-    description { FFaker::Lorem.paragraph }
+    description { FFaker::Lorem.characters(30) }
     points_required { 50 }
   end
 end
